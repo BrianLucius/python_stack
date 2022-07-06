7a2be55d6299978c6440558d3302e3def1030186
Brian Lucius <brian@brianlucius.net>
2022-07-05 22:35:46 -0600

    Add bank_account exercise

---
+++ fundamentals/oop/bank_account.py
@@ -0,0 +1,44 @@
+class BankAccount:
+    all_accounts = []
+    
+    def __init__(self, interest_rate, balance = 0):
+        self.balance = balance
+        self.interest_rate = interest_rate
+        BankAccount.all_accounts.append(self)
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdrawl(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print(f"Insufficient funds: Charging a $5 fee.")
+            self.balance -= 5
+        return self
+
+    def display_account_info(self):
+        print(f"The current balance is: ${self.balance}.")
+        return self
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.interest_rate)
+        return self
+
+    @classmethod
+    def print_bank_accounts(cls):
+        print("\nListing all accounts:")
+        for account in (cls.all_accounts):
+            print(f"The current balance is: ${account.balance} with an interest rate of: {account.interest_rate}%")
+        return
+    
+
+user1 = BankAccount(balance=10000, interest_rate=.10)
+user2 = BankAccount(interest_rate=.20)
+
+user1.deposit(100).deposit(300).deposit(75).yield_interest().display_account_info()
+user2.deposit(1245).deposit(412).withdrawl(11124).withdrawl(73).withdrawl(154).withdrawl(23).yield_interest().display_account_info()
+
+BankAccount.print_bank_accounts()
