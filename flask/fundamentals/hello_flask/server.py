48b5bfacb97a339b72329db3fb3224fc3763520c
Brian Lucius <brian@brianlucius.net>
2022-07-08 13:35:38 -0600

    Add lists and tables to hello_flask project

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -21,9 +21,19 @@ def hello(userName):
 def repeat(count, word):
     return render_template("hello.html",word=word, count=count)
 
+@app.route('/lists')
+def render_lists():
+    student_info = [
+        {'name': 'Michael', 'age': 35},
+        {'name': 'John', 'age': 30},
+        {'name': 'Mark', 'age': 25},
+        {'name': 'KB', 'age': 27}
+    ]
+    return render_template("lists.html", random_numbers=[3,1,5], students=student_info)
+
 @app.errorhandler(404)
 def notFound(e):
     return "Sorry! No response. Try again."
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+    app.run(debug=True, port=5001)    # Run the app in debug mode.
