f17e209b36aac72ee0d16ffe3d2d1c7a4cfd4234
Brian Lucius <brian@brianlucius.net>
2022-07-07 20:34:52 -0600

    Add Understanding Routing exercise

---
+++ flask/fundamentals/hello_flask/server.py
@@ -0,0 +1,26 @@
+from flask import Flask  # Import Flask to allow us to create our app
+
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello Python World!'  # Return the string 'Hello World!' as a response
+
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+
+@app.route('/say/<string:userName>')
+def hello(userName):
+    return f"Hi {userName}!"
+
+@app.route('/repeat/<int:count>/<string:word>')
+def repeat(count, word):
+    return f"{word * count}"
+
+@app.errorhandler(404)
+def notFound(e):
+    return "Sorry! No response. Try again."
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
