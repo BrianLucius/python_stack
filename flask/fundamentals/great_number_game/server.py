a9f430a9039f5246d0e519ed680e2a974913a311
Brian Lucius <brian@brianlucius.net>
2022-07-10 20:02:07 -0600

    Enhance number game

--- flask/fundamentals/great_number_game/server.py
+++ flask/fundamentals/great_number_game/server.py
@@ -9,6 +9,9 @@ def main():
     if 'random_num' not in session:
         print("Generating and storing random number...")
         session['random_num'] = random.randint(1, 100)
+        print(session['random_num'])
+    if 'num_guesses' not in session:
+        session['num_guesses'] = 0
     return render_template('index.html', hint="", random_num="")
 
 @app.route('/guess', methods=['POST'])
@@ -16,6 +19,7 @@ def guess():
     player_hint = ""
     winning_num = ""
     if request.form['guess']:
+        session['num_guesses'] = int(session['num_guesses']) + 1
         if int(request.form['guess']) > session['random_num']:
             player_hint = 'lower'
         elif int(request.form['guess']) < session['random_num']:
@@ -26,7 +30,10 @@ def guess():
         else:
             player_hint = ""
             winning_num = ""
-    return render_template('/index.html', hint = player_hint, random_num = winning_num)
+        if int(session['num_guesses']) == 5 and player_hint != 'match':
+            player_hint = 'lose'
+            winning_num = session['random_num']
+    return render_template('/index.html', hint = player_hint, random_num = winning_num, guesses = session['num_guesses'])
 
 @app.route('/play_again')
 def play_again():
