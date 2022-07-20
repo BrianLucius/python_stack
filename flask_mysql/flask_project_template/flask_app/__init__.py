49b4c649bcad5c99234b6391c6c679934998594e
Brian Lucius <brian@brianlucius.net>
2022-07-19 20:48:41 -0600

    Update template __init__

--- flask_mysql/flask_project_template/flask_app/__init__.py
+++ flask_mysql/flask_project_template/flask_app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+import re
 
+app = Flask(__name__)
 app.secret_key = "Supercalifragilisticexpealadocious!"
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 DATABASE = 'enter_db_name'
